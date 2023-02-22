0fdc19df967d4bbd61505b0a8d518b5aaf25ccee
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-02-22 10:43:05 +0530

    Commit

--- src/main/java/com/lti/demo/controller/EmployeeController.java
+++ src/main/java/com/lti/demo/controller/EmployeeController.java
@@ -1,5 +1,13 @@
 package com.lti.demo.controller;
 
+import com.lti.demo.model.Employee;
+
 public class EmployeeController {
+
+	public Employee emp() {
+		Employee emp = new Employee(101, "Sonu", 90000);
+		System.out.println(emp.toString());
+		return emp;
+	}
 
 }
--- src/main/java/com/lti/demo/model/Employee.java
+++ src/main/java/com/lti/demo/model/Employee.java
@@ -2,4 +2,24 @@ package com.lti.demo.model;
 
 public class Employee {
 
+	private int employeeId;
+	private String firstName;
+	private double salary;
+
+	public Employee() {
+		super();
+	}
+
+	public Employee(int employeeId, String firstName, double salary) {
+		super();
+		this.employeeId = employeeId;
+		this.firstName = firstName;
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [employeeId=" + employeeId + ", firstName=" + firstName + ", salary=" + salary + "]";
+	}
+
 }

8d219d9b17fb76767cf408997ee91a457ccc64c6
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-02-23 17:34:58 +0530

    Commit

--- src/main/java/com/lti/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/lti/demo/exception/GlobalExceptionHandler.java
@@ -1,18 +1,10 @@
 package com.lti.demo.exception;
 
-import java.util.List;
-import java.util.Locale;
-import java.util.stream.Collectors;
-
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.lti.demo.model.Department;
 import com.lti.demo.model.Employee;

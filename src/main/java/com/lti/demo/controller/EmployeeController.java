dcb826e7153c9f92ca7b3dce2f4574a7dae8e0cc
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-02-23 18:05:49 +0530

    Commit

--- src/main/java/com/lti/demo/controller/EmployeeController.java
+++ src/main/java/com/lti/demo/controller/EmployeeController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,8 @@ import com.lti.demo.model.Employee;
 import com.lti.demo.service.EmployeeService;
 
 @RestController
+//@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "localhost://3000")
 @RequestMapping(value = "/emp")
 public class EmployeeController {
 

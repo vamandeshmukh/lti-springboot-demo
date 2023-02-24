3775f1b4688d8ab1718850a9efa48bd842e750b1
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-02-24 16:48:32 +0530

    Commit

--- src/main/java/com/lti/demo/App.java
+++ src/main/java/com/lti/demo/App.java
@@ -3,10 +3,8 @@ package com.lti.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
 @SpringBootApplication
-//@EnableSwagger2
+
 public class App {
 
 	public static void main(String[] args) {
